54ef64af30878cc66fb9b378f2ee70cadee6a95e
Nelith Nethsanda <139336822+nelith-2002@users.noreply.github.com>
2026-02-18 01:23:56 +0530

    feat: add quiz and question screens with answer buttons

---
+++ quiz_app/lib/answer_btn.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AnswerBtn extends StatelessWidget {
+  const AnswerBtn({super.key, required this.answerText, required this.onTap});
+
+  final String answerText;
+  final void Function() onTap;
+  @override
+  Widget build(context) {
+    return ElevatedButton(
+      onPressed: onTap,
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 40),
+        backgroundColor: const Color.fromARGB(255, 33, 1, 95),
+        foregroundColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadiusGeometry.circular(10),
+        ),
+      ),
+      child: Text(answerText),
+    );
+  }
+}
--- quiz_app/lib/quiz_screen.dart
+++ quiz_app/lib/quiz_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_btn.dart';
+import 'package:quiz_app/data/question.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -10,8 +12,45 @@ class QuestionScreen extends StatefulWidget {
 }
 
 class _QuestionScreen extends State<QuestionScreen> {
+  var currentQuestionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      currentQuestionIndex++;
+    });
+  }
+
   @override
   Widget build(context) {
-    return const Text('Question Screen');
+    final currentQuestion = questions[currentQuestionIndex];
+
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: EdgeInsets.all(38),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+            ...currentQuestion.getShuffledAnswers().map((answer) {
+              return AnswerBtn(
+                answerText: answer,
+                onTap: answerQuestion,
+              ); // without being hard coded this will give btn dynamically according to the question.dart in the data folder
+            }),
+            // AnswerBtn(answerText: currentQuestion.answers[0], onTap: () {}),
+            // AnswerBtn(answerText: currentQuestion.answers[1], onTap: () {}),
+            // AnswerBtn(answerText: currentQuestion.answers[2], onTap: () {}),
+            // AnswerBtn(answerText: currentQuestion.answers[3], onTap: () {}),
+          ],
+        ),
+      ),
+    );
   }
 }
